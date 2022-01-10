2d6bd7124a2d501a44d70536e0a65ce72e582042
Matt Heere <mattheere@hotmail.com>
2022-01-10 13:55:55 -0500

    Add startup script

---
+++ src/rotctl.bat
@@ -0,0 +1,6 @@
+"cmd C:\Program Files (x86)\hamlib-w64-4.1\bin\rotctld.exe" -m 201 -r COM9 -s 19200
+REM "C:\Program Files (x86)\hamlib-w64-4.1\bin\rigctld.exe" -m 1003 -r COM10 -s 4800
+"cmd C:\Program Files (x86)\hamlib-w64-4.1\bin\rigctld.exe" -m 1 -r COM10 -s 4800
+"C:\SDRSharp\SDRSharp.exe"
+"C:\Program Files (x86)\gpredict-win32-2.3.37\gpredict.exe"
+
